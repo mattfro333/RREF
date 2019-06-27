997c47dd9cf9253b68ecb0303a511237a3e5b12c
mattfro333 <mattfro333@gmail.com>
2019-06-27 17:08:11 -0600

    endpoints worked but create didn't work

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,8 +1,42 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello Evolution!");
+});
+
+exports.getIcecream = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('icecream').get()
+    .then(data => {
+      let icecream =[];
+      data.forEach(doc => {
+        icecream.push(doc.data());
+      });
+      return res.json(icecream)
+    })
+    .catch(err => console.error(err))
+});
+
+exports.createIcecream = functions.https.onRequest((req, res) => {
+    const newIcecream = {
+      body: req.body.body,
+      userHandle: req.body.userHandle,
+      createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore()
+    .collection('icecream')
+    .add(newIcecream)
+    .then(doc => {
+      res.json({message: `document ${doc.id} created successfully`});
+    })
+    .catch(err => {
+      res.status(500).json({error: 'something went wront'});
+      console.error(err);
+    })
+});
